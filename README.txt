218c6cd6a256bf109b8e124b8ab7eeaa2a0f4d08
Hiroyoshi Uchiyama <hiroyoshi.uchiyama@gmail.com>
2018-12-12 00:59:34 +0900

    WpfApp1-2 画面の動作（＝ViewModel）を実装

---
+++ README.txt
@@ -0,0 +1,8 @@
+WpfApp1-1	画面だけ作ってみた
+WpfApp1-2	画面の動作（＝ViewModel）を実装
+WpfApp1-3	検索機能を実装
+WpfApp2		ViewModelのテストを実装
+WpfApp3-1	行番号と列番号の表示を実装
+WpfApp3-2	行番号と列番号の表示の色変えを実装
+WpfApp4		テキストの表示と編集を実装
+WpfApp5		テキストのリアルタイム検索を実装
---
+++ WpfApp1-2/ViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Prism.Commands;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private string _searchString;
+        private string _filePath;
+        private string _searchResult;
+
+        public ViewModel()
+        {
+            _searchString = "SearchString";
+            _filePath = "FilePath";
+            _searchResult = string.Empty;
+            SearchCommand = new DelegateCommand(
+                () => Search(),
+                () => string.IsNullOrEmpty(SearchString) && File.Exists(FilePath));
+        }
+
+        public string SearchString
+        {
+            get => _searchString;
+            set
+            {
+                if(_searchString != value)
+                {
+                    NotifyPropertyChanged();
+                    _searchString = value;
+                }
+            }
+        }
+
+        public string FilePath
+        {
+            get => _filePath;
+            set
+            {
+                if (_filePath != value)
+                {
+                    NotifyPropertyChanged();
+                    _filePath = value;
+                }
+            }
+        }
+
+        public string SearchResult
+        {
+            get => _searchResult;
+            set
+            {
+                if(_searchResult != value)
+                {
+                    NotifyPropertyChanged();
+                    _searchResult = value;
+                }
+            }
+        }
+
+        public DelegateCommand SearchCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null && propertyName != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void Search()
+        {
+        }
+    }
+}
