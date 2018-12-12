cc552abe7b5c3fd27c989c1ff7f8f12b912d842f
Hiroyoshi Uchiyama <hiroyoshi.uchiyama@gmail.com>
2018-12-12 22:07:15 +0900

    ちょっときれいに表示

--- README.txt
+++ README.txt
@@ -3,6 +3,7 @@ WpfApp1-2	画面の動作（＝ViewModel）を実装
 WpfApp1-3	検索機能を実装
 WpfApp2Test	ViewModelのテストを実装
 WpfApp3-1	行番号と列番号の表示を実装
+			https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/ms742521(v%3dvs.100)
 WpfApp3-2	ちょっときれいに表示
 WpfApp4		テキストの表示と編集を実装
 WpfApp5		テキストのリアルタイム検索を実装
---
+++ WpfApp3-2/ViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Prism.Commands;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace WpfApp
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _searchString;
+        private string _filePath;
+        private IEnumerable<MatchedLine> _searchResult;
+
+        public ViewModel()
+        {
+            _searchString = "SearchString";
+            _filePath = "FilePath";
+            _searchResult = Enumerable.Empty<MatchedLine>();
+            SearchCommand = new DelegateCommand(
+                () => Search(),
+                () => !string.IsNullOrEmpty(SearchString) && File.Exists(FilePath))
+                .ObservesProperty(() => SearchString)
+                .ObservesProperty(() => FilePath);
+        }
+
+        public string SearchString
+        {
+            get => _searchString;
+            set
+            {
+                if(_searchString != value)
+                {
+                    _searchString = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string FilePath
+        {
+            get => _filePath;
+            set
+            {
+                if (_filePath != value)
+                {
+                    _filePath = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public IEnumerable<MatchedLine> SearchResult
+        {
+            get => _searchResult;
+            set
+            {
+                if(_searchResult != value)
+                {
+                    _searchResult = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DelegateCommand SearchCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Search()
+        {
+            this.SearchResult = MatchedLines;
+        }
+
+        private IEnumerable<MatchedLine> MatchedLines
+        {
+            get
+            {
+                int lineNo = 0;
+                foreach (var line in File.ReadLines(this.FilePath))
+                {
+                    lineNo++;
+                    var m = Regex.Match(line, this.SearchString);
+                    if (m.Success)
+                    {
+                        yield return new MatchedLine(lineNo, m.Index, line);
+                    }
+                }
+            }
+        }
+    }
+
+    public class MatchedLine
+    {
+        public MatchedLine(int lineNo, int linePosition, string line)
+        {
+            LineNo = lineNo;
+            LinePosition = linePosition;
+            Line = line;
+        }
+
+        public int LineNo { get; }
+        public int LinePosition { get; }
+        public string Line { get; }
+    }
+}
